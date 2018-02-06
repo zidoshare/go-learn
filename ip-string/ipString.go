e4e99061c5ced730c0d7366f38f4a480f3222c96
邬虹旭 <wuhongxu1208@gmail.com>
2018-02-06 18:02:59 +0800

    add interface

---
+++ ip-string/ipString.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+//IPAddr ip地址
+type IPAddr [4]byte
+
+func (ipAddr IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3])
+}
+
+func main() {
+	addrs := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for n, a := range addrs {
+		fmt.Printf("%v: %v\n", n, a)
+	}
+}
